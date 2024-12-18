2e9b62da2e4607985d684207f72d704c27d8c7c6
Phemelo Moloi <moloieric80@gmail.com>
2024-12-18 11:18:37 +0200

    updated player and added background tiles to GamePanel

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -8,6 +8,7 @@ import java.awt.Graphics2D;
 import javax.swing.JPanel;
 
 import entity.Player;
+import tile.TileManager;
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -24,6 +25,7 @@ public class GamePanel extends JPanel implements Runnable {
     // FPS
     int FPS = 60;
 
+    TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
@@ -82,6 +84,7 @@ public class GamePanel extends JPanel implements Runnable {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
+        tileM.draw(g2);
         player.draw(g2);
         g2.dispose();
     }
