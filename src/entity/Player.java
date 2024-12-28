b5ca1e17050c2d95b48506bd2ac00025a5801a9c
Phemelo Moloi <moloieric80@gmail.com>
2024-12-28 13:16:16 +0200

    preparation for object update making interactive key objects

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -25,7 +25,13 @@ public class Player extends Entity {
         screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
         screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
 
-        solidArea = new Rectangle(8, 16, 32, 32);
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        solidArea.width = 32;
+        solidArea.height = 32;
 
         setDefaultValues();
         getPlayerImage();
