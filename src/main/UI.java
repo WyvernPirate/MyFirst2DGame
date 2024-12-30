12e6f93310749a3be1d3858ad5d66ea46490d39d
Phemelo Moloi <moloieric80@gmail.com>
2024-12-30 12:45:10 +0200

    implemented a timer for the game, to record time for game completion and to display to player

--- src/main/UI.java
+++ src/main/UI.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.text.DecimalFormat;
 
 import object.OBJ_Key;
 
@@ -16,6 +17,8 @@ public class UI {
     public String message = "";
     int messageCounter = 0;
     public boolean gameFinished = false;
+    double playTime;
+    DecimalFormat dFormat = new DecimalFormat("#0.00");
 
     public UI(GamePanel gp) {
         this.gp = gp;
@@ -48,6 +51,12 @@ public class UI {
             y = gp.screenHeight / 2 - (gp.tileSize * 3);
             g2.drawString(text, x, y);
 
+            text = "Your time is " + dFormat.format(playTime) + "!";
+            textLength = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
+            x = gp.screenWidth / 2 - textLength / 2;
+            y = gp.screenHeight / 2 + (gp.tileSize * 4);
+            g2.drawString(text, x, y);
+
             g2.setFont(arial_80B);
             g2.setColor(Color.YELLOW);
             text = "Congratulations";
@@ -63,6 +72,10 @@ public class UI {
             g2.setColor(Color.WHITE);
             g2.drawImage(keyImage, gp.tileSize / 2, gp.tileSize / 2, gp.tileSize, gp.tileSize, null);
             g2.drawString("x " + gp.player.hasKey, 74, 65);
+
+            // TIME
+            playTime += (double) 1 / 60;
+            g2.drawString("Time:" + dFormat.format(playTime), gp.tileSize * 11, 65);
 
             // MESSAGE
             if (messageOn == true) {
